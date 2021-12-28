edf8ba18659e0354c46ada7b46db5cc2ded7daa9
floh-bhaptics <floh@fahrenbergers.de>
2021-12-28 06:22:49 +0100

    Projektdateien hinzufügen.

---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using MelonLoader;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
+        private static ManualResetEvent Water_mrse = new ManualResetEvent(false);
+        private static ManualResetEvent Choking_mrse = new ManualResetEvent(false);
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+
+        private static bHaptics.RotationOption defaultRotationOption = new bHaptics.RotationOption(0.0f, 0.0f);
+
+        public void HeartBeatFunc()
+        {
+            while (true)
+            {
+                HeartBeat_mrse.WaitOne();
+                bHaptics.SubmitRegistered("HeartBeat");
+                Thread.Sleep(600);
+            }
+        }
+
+        public void WaterFunc()
+        {
+            while (true)
+            {
+                Water_mrse.WaitOne();
+                bHaptics.SubmitRegistered("WaterSlushing");
+                Thread.Sleep(5050);
+            }
+        }
+
+        public void ChokingFunc()
+        {
+            while (true)
+            {
+                Choking_mrse.WaitOne();
+                bHaptics.SubmitRegistered("Choking");
+                Thread.Sleep(1050);
+            }
+        }
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            if (!bHaptics.WasError)
+            {
+                suitDisabled = false;
+            }
+            RegisterAllTactFiles();
+            LOG("Starting HeartBeat and NeckTingle thread...");
+            Thread HeartBeatThread = new Thread(HeartBeatFunc);
+            HeartBeatThread.Start();
+            Thread WaterThread = new Thread(WaterFunc);
+            WaterThread.Start();
+            Thread ChokingThread = new Thread(ChokingFunc);
+            ChokingThread.Start();
+        }
+
+        public void LOG(string logStr)
+        {
+#pragma warning disable CS0618 // Typ oder Element ist veraltet
+            MelonLogger.Msg(logStr);
+#pragma warning restore CS0618 // Typ oder Element ist veraltet
+        }
+
+
+
+        void RegisterAllTactFiles()
+        {
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\bHaptics";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    bHaptics.RegisterFeedbackFromTactFile(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+            }
+            systemInitialized = true;
+            //PlaybackHaptics("HeartBeat");
+        }
+
+        public void PlaybackHaptics(String key, float intensity = 1.0f, float duration = 1.0f)
+        {
+            //LOG("Trying to play");
+            if (FeedbackMap.ContainsKey(key))
+            {
+                //LOG("ScaleOption");
+                bHaptics.ScaleOption scaleOption = new bHaptics.ScaleOption(intensity, duration);
+                //LOG("Submit");
+                bHaptics.SubmitRegistered(key, key, scaleOption, defaultRotationOption);
+                // LOG("Playing back: " + key);
+            }
+            else
+            {
+                LOG("Feedback not registered: " + key);
+            }
+        }
+
+        public void PlayBackHit(String key, float xzAngle, float yShift)
+        {
+            bHaptics.ScaleOption scaleOption = new bHaptics.ScaleOption(1f, 1f);
+            bHaptics.RotationOption rotationOption = new bHaptics.RotationOption(xzAngle, yShift);
+            bHaptics.SubmitRegistered(key, key, scaleOption, rotationOption);
+        }
+
+        public void Recoil(string weaponName, bool isRightHand, float intensity = 1.0f)
+        {
+            float duration = 1.0f;
+            var scaleOption = new bHaptics.ScaleOption(intensity, duration);
+            var rotationFront = new bHaptics.RotationOption(0f, 0f);
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+            string keyArm = "Recoil" + postfix;
+            string keyVest = "Recoil" + weaponName + "Vest" + postfix;
+            string keyHands = "RecoilHands" + postfix;
+            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
+        }
+
+
+        public void GunRecoil(bool isRightHand, float intensity = 1.0f )
+        {
+            float duration = 1.0f;
+            var scaleOption = new bHaptics.ScaleOption(intensity, duration);
+            var rotationFront = new bHaptics.RotationOption(0f, 0f);
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+            string keyArm = "Recoil" + postfix;
+            string keyVest = "RecoilVest" + postfix;
+            string keyHands = "RecoilHands" + postfix;
+            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
+        }
+
+        public void SwordRecoil(bool isRightHand, float intensity = 1.0f)
+        {
+            float duration = 1.0f;
+            var scaleOption = new bHaptics.ScaleOption(intensity, duration);
+            var rotationFront = new bHaptics.RotationOption(0f, 0f);
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+            string keyArm = "Sword" + postfix;
+            string keyVest = "SwordVest" + postfix;
+            string keyHands = "RecoilHands" + postfix;
+            bHaptics.SubmitRegistered(keyHands, keyHands, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyArm, keyArm, scaleOption, rotationFront);
+            bHaptics.SubmitRegistered(keyVest, keyVest, scaleOption, rotationFront);
+        }
+
+        public void HeadShot(String key, float hitAngle)
+        {
+            if (bHaptics.IsDeviceConnected(bHaptics.DeviceType.Tactal))
+            {
+                if ((hitAngle < 45f) | (hitAngle > 315f)) { PlaybackHaptics("Headshot_F"); }
+                if ((hitAngle > 45f) && (hitAngle < 135f)) { PlaybackHaptics("Headshot_L"); }
+                if ((hitAngle > 135f) && (hitAngle < 225f)) { PlaybackHaptics("Headshot_B"); }
+                if ((hitAngle > 225f) && (hitAngle < 315f)) { PlaybackHaptics("Headshot_R"); }
+            }
+            else { PlayBackHit(key, hitAngle, 0.5f); }
+        }
+
+        public void StartHeartBeat()
+        {
+            HeartBeat_mrse.Set();
+        }
+
+        public void StopHeartBeat()
+        {
+            HeartBeat_mrse.Reset();
+        }
+
+        public bool IsPlaying(String effect)
+        {
+            return bHaptics.IsPlaying(effect);
+        }
+
+        public void StopHapticFeedback(String effect)
+        {
+            bHaptics.TurnOff(effect);
+        }
+
+        public void StopAllHapticFeedback()
+        {
+            StopThreads();
+            foreach (String key in FeedbackMap.Keys)
+            {
+                bHaptics.TurnOff(key);
+            }
+        }
+
+        public void StopThreads()
+        {
+            StopHeartBeat();
+        }
+
+
+    }
+}
---
+++ ShadowLegend_bhaptics.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MelonLoader;
+using HarmonyLib;
+
+using MyBhapticsTactsuit;
+using UnityEngine;
+using VitruviusVR;
+
+namespace ShadowLegend_bhaptics
+{
+    public class ShadowLegend_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        private static PlayerHand rightHand;
+        private static bool weaponRightHand = true;
+        private static bool arrowFromCrossBow = false;
+
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(VitruviusVR.PlayerController), "OnEnable", new Type[] { })]
+        public class bhaptics_EnablePlayerController
+        {
+            [HarmonyPostfix]
+            public static void Postfix(VitruviusVR.PlayerController __instance)
+            {
+                rightHand = __instance.CurrentRightHand;
+            }
+        }
+
+        #region Weapon handling
+
+        [HarmonyPatch(typeof(PlayerMeleeDamage), "ApplyDamage", new Type[] { typeof(DamagableObject), typeof(Vector3) })]
+        public class bhaptics_MeleeDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerMeleeDamage __instance)
+            {
+                bool isRightHand = __instance.PlayerWeapon.Hand.Equals(rightHand);
+                float weaponSpeed = __instance.PlayerWeapon.WeaponTipVelocity.magnitude;
+                float intensity = Math.Min(((weaponSpeed / 80f) + 0.5f), 1.0f);
+                tactsuitVr.Recoil("Blade", isRightHand, intensity);
+                //tactsuitVr.LOG("MeleeDamage: " + weaponSpeed.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(HandCannon), "CannonInteractionStart", new Type[] { typeof(InteractableObject), typeof(PlayerHand) })]
+        public class bhaptics_GrabCannon
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerHand hand)
+            {
+                weaponRightHand = hand.Equals(rightHand);
+            }
+        }
+
+        [HarmonyPatch(typeof(HandCannon), "ShootCannon", new Type[] {  })]
+        public class bhaptics_ShootCannon
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.Recoil("Cannon", weaponRightHand);
+                //tactsuitVr.LOG("MeleeDamage: " + weaponSpeed.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(CrossBow), "InteractionStart", new Type[] { typeof(InteractableObject), typeof(PlayerHand) })]
+        public class bhaptics_GrabCrossBow
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerHand hand)
+            {
+                weaponRightHand = hand.Equals(rightHand);
+                arrowFromCrossBow = true;
+            }
+        }
+
+
+        [HarmonyPatch(typeof(CrossBow), "Shoot", new Type[] { })]
+        public class bhaptics_ShootCrossBow
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                // right hand is where arrow was
+                tactsuitVr.Recoil("Gun", !weaponRightHand);
+                arrowFromCrossBow = false;
+                //tactsuitVr.LOG("MeleeDamage: " + weaponSpeed.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(ArrowHand), "InteractionStart", new Type[] { typeof(InteractableObject), typeof(PlayerHand) })]
+        public class bhaptics_GrabArrowHand
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerHand hand)
+            {
+                weaponRightHand = hand.Equals(rightHand);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(ArrowHand), "ShootArrow", new Type[] { })]
+        public class bhaptics_ShootArrowHand
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                // Right hand: arrow!!!
+                if (!arrowFromCrossBow) { tactsuitVr.Recoil("Bow", weaponRightHand); }
+            }
+        }
+
+        #endregion
+
+        #region Player damage
+
+        private static KeyValuePair<float, float> getAngleAndShift(Transform player, Vector3 hit)
+        {
+            // bhaptics starts in the front, then rotates to the left. 0° is front, 90° is left, 270° is right.
+            Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+            // y is "up", z is "forward" in local coordinates
+            Vector3 hitPosition = hit - player.position;
+            Quaternion PlayerRotation = player.rotation;
+            Vector3 playerDir = PlayerRotation.eulerAngles;
+            Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+            float earlyhitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+            Vector3 earlycrossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+            if (earlycrossProduct.y > 0f) { earlyhitAngle *= -1f; }
+            tactsuitVr.LOG("EarlyHitAngle: " + earlyhitAngle.ToString());
+            float myRotation = earlyhitAngle - playerDir.y;
+            //myRotation *= -1f;
+            if (myRotation < 0f) { myRotation = 360f + myRotation; }
+            tactsuitVr.LOG("mHitAngle: " + myRotation.ToString());
+
+
+            float hitShift = hitPosition.y;
+            tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+            if (hitShift > 1.8f) { hitShift = 0.5f; }
+            else if (hitShift < 1.0f) { hitShift = -0.5f; }
+            else { hitShift = (hitShift - 1.0f) / 0.8f - 0.5f; }
+            //tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+            //tactsuitVr.LOG(" ");
+
+            //tactsuitVr.LOG("Relative x-z-position: " + relativeHitDir.x.ToString() + " "  + relativeHitDir.z.ToString());
+            //tactsuitVr.LOG("HitAngle: " + hitAngle.ToString());
+            //tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+
+            return new KeyValuePair<float, float>(myRotation, hitShift);
+        }
+
+
+        [HarmonyPatch(typeof(Stats), "TakeDamage", new Type[] { typeof(DamageAttack) })]
+        public class bhaptics_StatsTakeDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Stats __instance, DamageAttack attack)
+            {
+                // damage function is for all entities
+                if (!__instance.name.Contains("Player")) { return; }
+
+                string feedbackKey = "Impact";
+                switch (attack.AttackDamageType)
+                {
+                    case DamageType.Melee:
+                        feedbackKey = "BladeHit";
+                        break;
+                    case DamageType.Range:
+                        feedbackKey = "BulletHit";
+                        break;
+                    case DamageType.Fire:
+                        feedbackKey = "Burning";
+                        break;
+                    case DamageType.Explosion:
+                        feedbackKey = "Impact";
+                        break;
+                    default:
+                        break;
+                }
+
+                tactsuitVr.LOG("Damage: " + attack.AttackDamageType.ToString());
+                Vector3 hitPosition = attack.WorldHitPosition;
+                Transform playerPosition = __instance.transform;
+                var angleShift = getAngleAndShift(playerPosition, hitPosition);
+                tactsuitVr.PlayBackHit(feedbackKey, angleShift.Key, angleShift.Value);
+            }
+        }
+
+        #endregion
+
+        [HarmonyPatch(typeof(HealthNumber), "HealthChange", new Type[] { typeof(int) })]
+        public class bhaptics_PlayerHealth
+        {
+            [HarmonyPostfix]
+            public static void Postfix(HealthNumber __instance, int currentHealth)
+            {
+                if (currentHealth < 30) { tactsuitVr.StartHeartBeat(); }
+                else { tactsuitVr.StopHeartBeat(); }
+                //tactsuitVr.LOG("Heal: " + currentHealth.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(VitruviusVR.PlayerController), "BurningStart", new Type[] { typeof(IncendiaryObject) })]
+        public class bhaptics_Burning
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("Burning");
+                tactsuitVr.LOG("BurningStart");
+            }
+        }
+
+    }
+}
